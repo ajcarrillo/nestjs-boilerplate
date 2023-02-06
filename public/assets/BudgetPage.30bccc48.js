b756efa766ccea5026eac904c2d74b52ca454129
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2023-02-06 03:35:51 -0500

    Add frontend build

---
+++ public/assets/BudgetPage.30bccc48.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./QPage.f1048661.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{j as t,k as a,l as c,m as n,d as s}from"./index.09c0efc9.js";const _={};function m(f,i){const e=c("router-view");return n(),t(o,null,{default:a(()=>[s(e)]),_:1})}var d=r(_,[["render",m]]);export{d as default};
--- public/assets/BudgetPage.45014ff2.js
+++
@@ -1,1 +0,0 @@
-import{Q as o}from"./QPage.3c55272d.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{j as t,k as a,l as c,m as n,d as s}from"./index.1280ff09.js";const _={};function m(f,i){const e=c("router-view");return n(),t(o,null,{default:a(()=>[s(e)]),_:1})}var d=r(_,[["render",m]]);export{d as default};
