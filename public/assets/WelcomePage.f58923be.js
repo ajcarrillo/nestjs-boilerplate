103d73d7a22b6e854d7ff0b2707fa6e196d6f2fa
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2023-02-06 00:59:54 -0500

    Add the spa app to the public folder

---
+++ public/assets/WelcomePage.f58923be.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{am as r,m as o}from"./index.812f81a0.js";const c={};function n(a,t){return o(),r("h1",null,"Bienvenido")}var s=e(c,[["render",n]]);export{s as default};
