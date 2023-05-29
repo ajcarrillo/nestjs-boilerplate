428d122bf188d83062f94d6d269cfd9d9fd45fb5
Andrés Carrillo <andresjch2804@gmail.com>
2023-05-28 21:23:26 -0500

    build: New frontend compilation

---
+++ public/assets/ArticleResource.5cc93ba2.js
@@ -0,0 +1,1 @@
+import{p as r}from"./planeacion.api.0342f2e3.js";var a={getArticles(){return r.get("/articles")},storeArticle(t){return r.post("/articles",t)},updateArticle(t,e){return r.put(`/articles/${t}`,e)},getDictionary(){return r.get("/articles/dictionary")}};export{a as A};
---
+++ public/assets/useMoney.dbc60660.js
@@ -0,0 +1,1 @@
+const e=()=>({currencyFormat:r=>{const n={style:"currency",currency:"MXN"};return new Intl.NumberFormat("es-MX",n).format(r)}});export{e as u};
