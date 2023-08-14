ec01ebaa966878717c936d8c3406651d477271c0
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2023-08-14 02:45:50 -0500

    new frontend build

--- public/assets/QCardSection.94464a72.js
+++
@@ -1,1 +0,0 @@
-import{v as e,c as r,h as n,x as s}from"./index.e2638eb4.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
---
+++ public/assets/useGoBack.c7f4c8f8.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.fd322d53.js";function s(){const o=e();return{goBack:()=>{window.history.length>1?o.go(-1):o.push({name:"welcome"})}}}export{s as u};
