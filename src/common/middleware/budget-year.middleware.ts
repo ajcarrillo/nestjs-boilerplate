d9a3896e1036dcc62e3514884ff05c76f6f25bc9
ajcarrilloSalvo <andres.carrillo@salvosoftware.com>
2024-04-27 19:44:16 -0500

    refact: Revamp the budget year middleware

--- src/common/middleware/budget-year.middleware.ts
+++ src/common/middleware/budget-year.middleware.ts
@@ -1,13 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class BudgetYearMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     const currentYear = new Date().getFullYear().toString();
-    const budgetYear = req.headers['x-budget-year'] as string || currentYear;
-
-    req['budgetYear'] = budgetYear;
+    req['budgetYear'] = req.headers['x-budget-year'] as string || currentYear;
 
     next();
   }
