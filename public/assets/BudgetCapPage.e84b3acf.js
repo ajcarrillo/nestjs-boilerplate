cd9d7735c2cb1aa61b7e842ac131c889783b445f
Andrés Carrillo <andresjch2804@gmail.com>
2024-09-29 21:39:05 -0500

    New frontend version

--- public/assets/BudgetCapPage.e84b3acf.js
+++
@@ -1,1 +0,0 @@
-import{Q as o}from"./QPage.6112cc6b.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{k as t,l as a,n,p as c,d as s}from"./index.33aee9c3.js";const _={};function p(f,m){const e=n("router-view");return c(),t(o,null,{default:a(()=>[s(e)]),_:1})}var d=r(_,[["render",p]]);export{d as default};
---
+++ public/assets/QSpace.607074b7.js
@@ -0,0 +1,1 @@
+import{h as e,v as a}from"./index.98b8b759.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
